aaf7a6f516eddb61b3ad47515560d4e28029bb6c
infinite0714 <infinite940714@outlook.com>
2024-03-06 17:40:00 +0200

    autorun server

---
+++ runotis.bat
@@ -0,0 +1,3 @@
+start cmd /k runxampp.bat
+start cmd /k rundjango.bat
+start cmd /k runreact.bat
---
+++ runxampp.bat
@@ -0,0 +1,4 @@
+@echo off
+
+echo Starting XAMPP MySQL Server...
+start "MySQL Server" cmd /c cd /D "C:\xampp\mysql\bin" && mysqld --console
